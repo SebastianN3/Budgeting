2f7c4e59d001dd57208d2d0d5ddbe049458c215c
SebastianN3 <navassebas@gmail.com>
2020-07-27 16:36:04 -0400

    Added initial HTML file

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Hello World!"
+    # You don't have to specify the name of the folder. 
+    # It knows it has to look into templates folder
+    return render_template("index.html")
 
 
 if __name__ == '__main__':
